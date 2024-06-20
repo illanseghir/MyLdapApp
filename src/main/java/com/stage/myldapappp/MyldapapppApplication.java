f8f418af6f387268e06a0463561829b8c1d28aeb
illanseghir <illanseghir0@gmail.com>
2024-06-20 13:39:30 +0200

    Initial commit

---
+++ src/main/java/com/stage/myldapappp/MyldapapppApplication.java
@@ -0,0 +1,13 @@
+package com.stage.myldapappp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyldapapppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyldapapppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stage/myldapappp/MyldapapppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stage.myldapappp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyldapapppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
